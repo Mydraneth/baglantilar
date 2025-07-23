d118a7e4709dd14fc5b57a8516677593883f985d
Mydraneth <keremyeles@gmail.com>
2025-07-23 19:21:15 +0300

    header done

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <header className="header">
+            {/* <img
+                className="logo"
+                src="src\assets\esle-me.png"
+                alt="Eşle-mi logo"
+            /> */}
+            <img
+                className="logo"
+                src="src\assets\ne-alaka.png"
+                alt="Eşle-mi logo"
+            />
+            <h1>Ne Alaka?!</h1>
+            {/* <h1>Eşle-ME!</h1> */}
+        </header>
+    );
+}
